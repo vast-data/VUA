ca7943eb8538e93e761fbe1560cf1703acab4014
Dan Aloni <dan.aloni@vastdata.com>
2025-04-22 15:55:49 +0300

    example: stress the fact it demos the KV Connector V0 interface

--- example/serve-vllm.sh
+++ example/serve-vllm.sh
@@ -7,7 +7,7 @@ model=${SERVE_MODEL:-mistralai/Mistral-7B-Instruct-v0.3}
 
 export PYTHONPATH=$(pwd)/example
 
-PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python example/vllm-wrap.py serve ${model} --cpu-offload-gb 15 --api-key ${api_key} \
+VLLM_USE_V1=0 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python example/vllm-wrap.py serve ${model} --cpu-offload-gb 15 --api-key ${api_key} \
     --enforce-eager \
     --kv-transfer-config \
     '{"kv_connector":"RemoteStorageConnector","kv_role":"kv_both","kv_rank":1,"kv_parallel_size":1,"kv_buffer_size":5e9}'
--- example/vllm-wrap.py
+++ example/vllm-wrap.py
@@ -9,11 +9,11 @@ print("Loading...")
 
 from vua.core import VUA, VUAConfig
 from vllm.distributed.kv_transfer.kv_connector.factory import KVConnectorFactory
-import vllm_connector
+import vllm_kv_connector_0
 from vllm.entrypoints.cli.main import main
 
 KVConnectorFactory.register_connector("RemoteStorageConnector",
-                                      "vllm_connector",
+                                      "vllm_kv_connector_0",
                                       "RemoteStorageConnector")
 
 kvcache_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),
@@ -25,7 +25,7 @@ except OSError:
     pass
 
 print("VUA path", kvcache_path)
-vllm_connector.vuacache = VUA(VUAConfig, kvcache_path)
+vllm_kv_connector_0.vuacache = VUA(VUAConfig, kvcache_path)
 
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
